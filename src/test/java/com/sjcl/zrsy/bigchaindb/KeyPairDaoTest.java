56411e6598786cce38c2e2a653853030c55bf2ab
nowto <now_to@tom.com>
2018-09-29 14:02:58 +0800

    unit test assert

--- src/test/java/com/sjcl/zrsy/bigchaindb/KeyPairDaoTest.java
+++ src/test/java/com/sjcl/zrsy/bigchaindb/KeyPairDaoTest.java
@@ -1,10 +1,13 @@
 package com.sjcl.zrsy.bigchaindb;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import java.io.File;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -25,12 +28,14 @@ public class KeyPairDaoTest {
     @Test
     public void testSave() {
         keyPairDao.save(keyPair, password);
+        Assert.assertTrue(new File(KeyPairDao.PRIKEY_FILE).exists());
+        Assert.assertTrue(new File(KeyPairDao.PUBKEY_FILE).exists());
     }
 
     @Test
     public void testGet() {
         KeyPair keyPair =  keyPairDao.get(password);
-        System.out.println(keyPair);
+        Assert.assertTrue(keyPair != null);
     }
 
     public static class KeyPairDaoStub extends KeyPairDao {
@@ -43,5 +48,11 @@ public class KeyPairDaoTest {
         protected byte[] getPriEncoded(String password) throws IOException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
             return keyPair.getPrivate().getEncoded();
         }
+    }
+
+    @AfterClass
+    public static void clear() {
+        new File(KeyPairDao.PRIKEY_FILE).delete();
+        new File(KeyPairDao.PUBKEY_FILE).delete();
     }
 }
