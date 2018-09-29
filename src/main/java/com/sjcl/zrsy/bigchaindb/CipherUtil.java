762397df93e774c6a841b7f6915f486caa4b9d0b
nowto <now_to@tom.com>
2018-09-29 12:11:22 +0800

    CipherUtil

--- src/main/java/com/sjcl/zrsy/bigchaindb/CipherUtil.java
+++ src/main/java/com/sjcl/zrsy/bigchaindb/CipherUtil.java
@@ -4,12 +4,14 @@ import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 class CipherUtil {
     private static final String CIPHER_ALGORITHM = "AES";
     private static final String CIPHER_ALGORITHM_STR =  CIPHER_ALGORITHM + "/ECB/PKCS5Padding";
 
     private static final Cipher cipher;
+    public static final int PASSWD_LEN = 16;
 
     private CipherUtil(){}
 
@@ -24,7 +26,7 @@ class CipherUtil {
     }
 
     public static byte[] encrypt(byte[] source, String password) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKey secretKey = new SecretKeySpec(password.getBytes(), CIPHER_ALGORITHM);
+        SecretKey secretKey = new SecretKeySpec(to16byte(password), CIPHER_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         cipher.update(source);
 
@@ -32,8 +34,18 @@ class CipherUtil {
     }
 
     public static byte[] decrypt(byte[] dest, String password) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKey secretKey = new SecretKeySpec(password.getBytes(), CIPHER_ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        SecretKey secretKey = new SecretKeySpec(to16byte(password), CIPHER_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return cipher.doFinal(dest);
+    }
+
+    private static byte[] to16byte(String password) {
+        if (password.length() == PASSWD_LEN) {
+            return password.getBytes();
+        }
+
+        int pwdLen = password.length();
+        String password16Byte = new String(new char[PASSWD_LEN - pwdLen]).replace("\0", "0").concat(password);
+        return password16Byte.getBytes();
     }
 }
--- src/test/java/com/sjcl/zrsy/bigchaindb/CipherUtilTest.java
+++ src/test/java/com/sjcl/zrsy/bigchaindb/CipherUtilTest.java
@@ -9,23 +9,33 @@ import java.security.InvalidKeyException;
 import java.util.Arrays;
 
 public class CipherUtilTest {
+
+    private byte[] data = {104, 101, 108, 108, 111, 119, 111, 114, 108, 100};
+    private byte[] expectedCode = {-29, -80, 44, 85, -81, 18, 76, 17, 87, 123, -80, 115, 46, 73, 33, 71};
+
+    private String password = "justtest";
+    private String wrongPassword = "testjust";
+
     @Test
-    public void testCipher() throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
-        String data = "helloworld";
-        String password = "justtestjusttest";
-
-        byte[] code = CipherUtil.encrypt(data.getBytes(), password);
-
-        Assert.assertTrue(!Arrays.equals(data.getBytes(), code));
-
-        String wrongPassword = "testjusttestjust";
-        byte[] wrongSource = CipherUtil.decrypt(code, wrongPassword);
-
-        Assert.assertTrue(!Arrays.equals(wrongSource, data.getBytes()));
-
-        byte[] source = CipherUtil.decrypt(code, password);
-
-        System.out.println(new String(source));
-        Assert.assertTrue(Arrays.equals(source, data.getBytes()));
+    public void testEncrypt() throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
+        byte[] code = CipherUtil.encrypt(data, password);
+        System.out.println(Arrays.toString(code));
+        Assert.assertTrue(Arrays.equals(expectedCode, code));
     }
+
+    @Test(expected = BadPaddingException.class)
+    public void testDecrypt_wrongPassword_() throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
+        byte[] wrongSource = CipherUtil.decrypt(expectedCode, wrongPassword);
+        Assert.assertTrue(!Arrays.equals(wrongSource, data));
+    }
+
+    @Test
+    public void testDecrypt_increctPassword_() throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException {
+        byte[] source = CipherUtil.decrypt(expectedCode, password);
+        Assert.assertTrue(Arrays.equals(source, data));
+    }
+
+
+
+
 }
