6c9ec14298abbd346ca28bbc298c43b44eae533b
nowto <now_to@tom.com>
2018-10-08 20:30:38 +0800

    add testBigchaindbDataToBean method

--- src/test/java/com/sjcl/zrsy/bigchaindb/BigchaindbUtilTest.java
+++ src/test/java/com/sjcl/zrsy/bigchaindb/BigchaindbUtilTest.java
@@ -3,8 +3,10 @@ package com.sjcl.zrsy.bigchaindb;
 import com.bigchaindb.builders.BigchainDbConfigBuilder;
 import com.bigchaindb.model.Transaction;
 import com.bigchaindb.util.KeyPairUtils;
+import com.google.gson.internal.LinkedTreeMap;
 import com.sjcl.zrsy.domain.dto.BigchaindbData;
 import com.sjcl.zrsy.domain.dto.RoleLogin;
+import com.sjcl.zrsy.domain.po.Operation;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -69,5 +71,25 @@ public class BigchaindbUtilTest {
         String id = BigchaindbUtil.getLastTransactonId(assetId);
 
         Assert.assertNotNull(id);
+    }
+
+    @Test
+    public void testBigchaindbDataToBean() throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        LinkedTreeMap op = new LinkedTreeMap();
+        op.put("content", "testBigchaindbDataToBean-content");
+        op.put("operation", "testBigchaindbDataToBean-operation");
+        op.put("id", "testBigchaindbDataToBean-id");
+        op.put("remark", "testBigchaindbDataToBean-remark");
+        op.put("time", "testBigchaindbDataToBean-time");
+
+        LinkedTreeMap<String, Object> data = new LinkedTreeMap<>();
+        data.put("type", Operation.class.getCanonicalName());
+        data.put("data", op);
+
+
+        Object bean = BigchaindbUtil.bigchaindbDataToBean(data);
+        Assert.assertTrue(bean instanceof Operation);
+        Operation opObj = (Operation) bean;
+        Assert.assertEquals("testBigchaindbDataToBean-content", opObj.getContent());
     }
 }
