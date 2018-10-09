8798d45efea68a440a0208b50786f4ea2d820d25
nowto <now_to@tom.com>
2018-10-09 16:40:33 +0800

    fix: cipher bug

--- src/main/java/com/sjcl/zrsy/bigchaindb/CipherUtil.java
+++ src/main/java/com/sjcl/zrsy/bigchaindb/CipherUtil.java
@@ -1,18 +1,24 @@
 package com.sjcl.zrsy.bigchaindb;
 
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 class CipherUtil {
-    private static final String CIPHER_ALGORITHM = "AES";
-    private static final String CIPHER_ALGORITHM_STR =  CIPHER_ALGORITHM + "/ECB/PKCS5Padding";
+    private static final byte[] SALT = new byte[]{2, 55, 11, 127, 66, 110, 77, 3};
+    private static final String CIPHER_ALGORITHM_STR = "PBEWITHMD5andDES";
+    public static final int ITERATION_COUNT = 100;
 
     private static final Cipher cipher;
-    public static final int PASSWD_LEN = 16;
 
-    private CipherUtil(){}
+    private CipherUtil() {
+    }
 
     static {
         try {
@@ -24,27 +30,23 @@ class CipherUtil {
         }
     }
 
-    public static byte[] encrypt(byte[] source, String password) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKey secretKey = new SecretKeySpec(to16byte(password), CIPHER_ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        cipher.update(source);
 
-        return cipher.doFinal();
+    private static SecretKey toKey(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(CIPHER_ALGORITHM_STR);
+        PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+        SecretKey secretKey = keyFactory.generateSecret(keySpec);
+        return secretKey;
     }
 
-    public static byte[] decrypt(byte[] dest, String password) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKey secretKey = new SecretKeySpec(to16byte(password), CIPHER_ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+    public static byte[] encrypt(byte[] source, String password) throws GeneralSecurityException {
+        SecretKey secretKey = toKey(password);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new PBEParameterSpec(SALT, ITERATION_COUNT));
+        return cipher.doFinal(source);
+    }
+
+    public static byte[] decrypt(byte[] dest, String password) throws GeneralSecurityException {
+        SecretKey secretKey = toKey(password);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, new PBEParameterSpec(SALT, ITERATION_COUNT));
         return cipher.doFinal(dest);
-    }
-
-    private static byte[] to16byte(String password) {
-        if (password.length() == PASSWD_LEN) {
-            return password.getBytes();
-        }
-
-        int pwdLen = password.length();
-        String password16Byte = new String(new char[PASSWD_LEN - pwdLen]).replace("\0", "0").concat(password);
-        return password16Byte.getBytes();
     }
 }
