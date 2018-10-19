b0962338a5ac720d3f5281c451993cc5e6cb17f3
nowto <now_to@tom.com>
2018-10-19 10:06:20 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/dao/ILogisticsOperationDao.java
+++ src/main/java/com/sjcl/zrsy/dao/ILogisticsOperationDao.java
@@ -2,11 +2,14 @@ package com.sjcl.zrsy.dao;
 
 import com.sjcl.zrsy.domain.dto.LogisticsOperation;
 
+/**
+ * LogisticsOperation data access object
+ */
 public interface ILogisticsOperationDao {
     /**
      * insert logistics operation
-     * @param logisticsOperation
-     * @return
+     * @param logisticsOperation to be insert to database
+     * @return <tt>true</tt> if inserted successfully
      */
     boolean insertLogisticsOperation(LogisticsOperation logisticsOperation);
 }
