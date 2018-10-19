a2911c6b1d510c29830e68851a10fc3082a517c1
nowto <now_to@tom.com>
2018-10-19 10:26:11 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/service/IRoleService.java
+++ src/main/java/com/sjcl/zrsy/service/IRoleService.java
@@ -1,12 +1,23 @@
 package com.sjcl.zrsy.service;
 
-import com.sjcl.zrsy.domain.dto.RoleLogin;
 import com.sjcl.zrsy.domain.po.Registration;
 
+/**
+ * role service
+ */
 public interface IRoleService {
-
-
+    /**
+     * registration.
+     *
+     * @param registration
+     * @return <tt>true</tt> if success
+     */
     boolean registration(Registration registration);
 
+    /**
+     * login
+     * @param registrationId
+     * @return
+     */
     Registration login(String  registrationId) ;
 }
