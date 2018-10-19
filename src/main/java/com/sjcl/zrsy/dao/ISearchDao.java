814c4830481a6e946d45b1c10a07b573f8c6f15a
nowto <now_to@tom.com>
2018-10-19 10:14:16 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/dao/ISearchDao.java
+++ src/main/java/com/sjcl/zrsy/dao/ISearchDao.java
@@ -2,6 +2,14 @@ package com.sjcl.zrsy.dao;
 
 import com.sjcl.zrsy.domain.dto.SearchId;
 
+/**
+ * search data access dao
+ */
 public interface ISearchDao {
+    /**
+     * search by pigId
+     * @param id pigId
+     * @return search
+     */
     SearchId select(String id);
 }
