6798075d4e2d784c51fe1eaf5fbdb449bf8dc37a
nowto <now_to@tom.com>
2018-10-19 10:24:09 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/service/IMarketService.java
+++ src/main/java/com/sjcl/zrsy/service/IMarketService.java
@@ -3,8 +3,22 @@ package com.sjcl.zrsy.service;
 import com.sjcl.zrsy.domain.dto.MarketReception;
 import com.sjcl.zrsy.domain.po.Operation;
 
+/**
+ * market service
+ */
 public interface IMarketService {
+
+    /**
+     * market receive a pig
+     * @param marketReception receive object
+     * @return <tt>true</tt> if seccess
+     */
     boolean marketreception(MarketReception marketReception);
 
+    /**
+     * market operation a pig
+     * @param marketOperation
+     * @return <tt>true</tt> if seccess
+     */
     boolean marketoperation(Operation marketOperation);
 }
