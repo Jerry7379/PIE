6359d23796491d1424f60c6b55e3528c9eb62ae5
nowto <now_to@tom.com>
2018-10-19 10:09:14 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/dao/IOperationDao.java
+++ src/main/java/com/sjcl/zrsy/dao/IOperationDao.java
@@ -4,12 +4,35 @@ import com.sjcl.zrsy.domain.po.Operation;
 
 import java.util.List;
 
+/**
+ * Operation data access object
+ */
 public interface IOperationDao {
+    /**
+     * insert farm operation
+     * @param operation farmOperation
+     * @return <tt>true</tt> if inserted successfully
+     */
     boolean insertFarmOperation(Operation operation);
 
+    /**
+     * insert market operation
+     * @param operation marketOperation
+     * @return <tt>true</tt> if inserted successfully
+     */
     boolean insertMarketOperation(Operation operation);
 
+    /**
+     * insert farm slaughter operation
+     * @param operation slaughterOperation
+     * @return <tt>true</tt> if inserted successfully
+     */
     boolean insertSlaughteroperartion(Operation operation);
 
+    /**
+     * find all operation by PigId
+     * @param pigId pigId
+     * @return operations
+     */
     List<Operation> findallOperationByPigid(String pigId);
 }
