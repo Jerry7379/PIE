97954c08feb6991b9f524822327ba56d800d6f0a
nowto <now_to@tom.com>
2018-10-19 10:27:08 +0800

    add comments

--- src/main/java/com/sjcl/zrsy/service/ISearchService.java
+++ src/main/java/com/sjcl/zrsy/service/ISearchService.java
@@ -3,7 +3,15 @@ package com.sjcl.zrsy.service;
 import com.sjcl.zrsy.domain.dto.SearchId;
 
 
+/**
+ * search service
+ */
 public interface ISearchService  {
 
+    /**
+     * obtain search result by pigId
+     * @param id pigId
+     * @return search result
+     */
     SearchId get(String id);
 }
