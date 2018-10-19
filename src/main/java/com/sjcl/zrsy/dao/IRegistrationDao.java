5450b8d23601d3e54cc9358fbf0f110cdd58cf91
nowto <now_to@tom.com>
2018-10-19 10:12:13 +0800

    add comment

--- src/main/java/com/sjcl/zrsy/dao/IRegistrationDao.java
+++ src/main/java/com/sjcl/zrsy/dao/IRegistrationDao.java
@@ -2,8 +2,21 @@ package com.sjcl.zrsy.dao;
 
 import com.sjcl.zrsy.domain.po.Registration;
 
+/**
+ * Registration data access object
+ */
 public interface IRegistrationDao {
+    /**
+     * obtain registration by registration id
+     * @param registrationId registration id
+     * @return registration
+     */
     Registration getLoginByRegistrationId(String registrationId);
 
+    /**
+     * insert registration to database
+     * @param registration registration
+     * @return <tt>true</tt> if inserted successfully
+     */
     boolean insertRegistration(Registration registration);
 }
