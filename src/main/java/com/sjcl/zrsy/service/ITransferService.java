149eb7eafcc7adeb28805bff823ed4e2ebd50f84
nowto <now_to@tom.com>
2018-10-19 10:31:07 +0800

    add comments

--- src/main/java/com/sjcl/zrsy/service/ITransferService.java
+++ src/main/java/com/sjcl/zrsy/service/ITransferService.java
@@ -1,5 +1,17 @@
 package com.sjcl.zrsy.service;
 
+/**
+ * transfer servcie.
+ *
+ * transfer pig from one registration to next registration
+ */
 public interface ITransferService {
+
+    /**
+     * transfer pig to publicKeyInHex
+     * @param pigId pigId
+     * @param publicKeyInHex publicKeyInHex
+     * @throws Exception
+     */
     void transfer(String pigId, String publicKeyInHex) throws Exception;
 }
