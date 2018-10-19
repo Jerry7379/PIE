39eca31d4b2212ce9c7b3a3d3176d0a77ac07c65
nowto <now_to@tom.com>
2018-10-19 10:16:23 +0800

    add comments

--- src/main/java/com/sjcl/zrsy/dao/ITransferDao.java
+++ src/main/java/com/sjcl/zrsy/dao/ITransferDao.java
@@ -1,5 +1,14 @@
 package com.sjcl.zrsy.dao;
 
+/**
+ * transfer data access object
+ */
 public interface ITransferDao {
+    /**
+     * transfer pig to publickeyInHex
+     * @param pigId pigId
+     * @param publicKeyInHex publicKeyInHex
+     * @throws Exception
+     */
     void transfer(String pigId, String publicKeyInHex) throws Exception;
 }
