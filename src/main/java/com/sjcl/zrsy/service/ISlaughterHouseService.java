c523efe563625f8e38fd1b45b19c43d20638f268
nowto <now_to@tom.com>
2018-10-19 10:29:11 +0800

    add comments

--- src/main/java/com/sjcl/zrsy/service/ISlaughterHouseService.java
+++ src/main/java/com/sjcl/zrsy/service/ISlaughterHouseService.java
@@ -3,7 +3,21 @@ package com.sjcl.zrsy.service;
 import com.sjcl.zrsy.domain.dto.SlaughterReception;
 import com.sjcl.zrsy.domain.dto.SlaughterOperation;
 
+/**
+ * slaughter house service
+ */
 public interface ISlaughterHouseService {
-    boolean slaughterreception(SlaughterReception slaughterReception);//屠宰检查
+    /**
+     * slaughter receive a pig
+     * @param slaughterReception receive object
+     * @return <tt>true</tt> if seccess
+     */
+    boolean slaughterreception(SlaughterReception slaughterReception);
+
+    /**
+     * slaughter operation a pig
+     * @param slaughterOperation
+     * @return <tt>true</tt> if seccess
+     */
     boolean slaughteroperation(SlaughterOperation slaughterOperation);
 }
